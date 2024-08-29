a87ae8f459c2f42289bf4b5c61bbb91f6213c8da
Ali Aizad <aliaizad72@gmail.com>
2024-08-29 13:56:36 +0800

    User can now attend events

--- app/controllers/event_attendees_controller.rb
+++ app/controllers/event_attendees_controller.rb
@@ -1,4 +1,8 @@
 class EventAttendeesController < ApplicationController
+  before_action :authenticate_user!, only: :attend
   def attend
+    @event = Event.find_by(id: params[:id])
+    @event.attendees << current_user
+    redirect_to event_path(params[:id])
   end
 end
