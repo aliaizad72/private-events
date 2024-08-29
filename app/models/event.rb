e302e0e4a425834d16059f1d1e7ef86889033807
Ali Aizad <aliaizad72@gmail.com>
2024-08-29 11:52:50 +0800

    Add many-to-many associations between attendees, event_attendees

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: "User", foreign_key: "creator_id"
+  has_many :event_attendees, foreign_key: :attended_event_id
+  has_many :attendees, through: :event_attendees
 
   validates :name, presence: true, length: { maximum: 150 }
   validates :event_time, presence: true, length: { maximum: 50 }
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :created_events, class_name: "Event", foreign_key: "creator_id", inverse_of: :creator
+  has_many :event_attendees, foreign_key: :attendee_id
+  has_many :attended_events, through: :event_attendees
 
   validates :name, presence: true
 end
